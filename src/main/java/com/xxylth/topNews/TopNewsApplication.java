e4e3949ef9ad1d003611e7debbe2d8e1cd66154f
XXY <547139255@qq.com>
2017-05-24 10:45:10 +0800

    项目搭建

---
+++ src/main/java/com/xxylth/topNews/TopNewsApplication.java
@@ -0,0 +1,12 @@
+package com.xxylth.topNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopNewsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xxylth/topNews/TopNewsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.xxylth.topNews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TopNewsApplication.class)
+@WebAppConfiguration
+public class TopNewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
