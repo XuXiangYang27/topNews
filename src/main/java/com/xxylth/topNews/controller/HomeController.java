ed9b0fd715808b4fa0fc5173ee3e98a829d41905
XXY <547139255@qq.com>
2017-06-01 15:11:38 +0800

    首页搭建完毕

--- src/main/java/com/xxylth/topNews/controller/HomeController.java
+++ src/main/java/com/xxylth/topNews/controller/HomeController.java
@@ -7,6 +7,7 @@ import com.xxylth.topNews.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,12 +30,10 @@ public class HomeController
     @Autowired
     UserService userService;
 
-    @RequestMapping(path = {"/","/index"},method = {RequestMethod.GET,RequestMethod.POST})//设置访问路径
-    public String index(Model model)
+    private List<ViewObject> getNews(int userId,int offset,int limit)
     {
-        System.out.println("222222222222222");
-        List<News> newsList=newsService.getLastedNews(0,0,10);
-        System.out.println("11111111111111");
+        List<News> newsList=
+                newsService.getLastedNews(userId,offset,limit);
         List<ViewObject> vos=new ArrayList<>();
         for(News news:newsList)
         {
@@ -43,10 +42,24 @@ public class HomeController
             vo.set("user", userService.getUser(news.getUserId()));
             vos.add(vo);
         }
-        model.addAttribute("vos",vos);
+        return vos;
+    }
+
+    @RequestMapping(path = {"/","/index"},method = {RequestMethod.GET,RequestMethod.POST})//设置访问路径
+    public String index(Model model)
+    {
+
+        model.addAttribute("vos",getNews(0,0,10 ));
         return "home";
     }
 
+    @RequestMapping(path = {"/user/{userId}"},method = {RequestMethod.GET,RequestMethod.POST})//设置访问路径
+    public String userIndex(Model model, @PathVariable("userId") int userId)
+    {
+
+        model.addAttribute("vos",getNews(userId,0,10 ));
+        return "home";
+    }
 }
 
 
